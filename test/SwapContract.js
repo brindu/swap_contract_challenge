aa2bfdbfc26b29fddce2490e6a4a71fa8b7365f2
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-07-24 16:58:45 +0200

    Refactor tests

--- test/SwapContract.js
+++ test/SwapContract.js
@@ -11,6 +11,8 @@ contract("SwapContract", accounts => {
   let toToken;
   let swapContract;
 
+  const mintInitialSupply = async (token, wallet) => { await token.mintSupplyFor(wallet, new BN('10000')) };
+
   beforeEach(async () => {
     fromToken = await FromToken.new();
     toToken = await ToToken.new();
@@ -18,7 +20,7 @@ contract("SwapContract", accounts => {
 
     // The SwapContract contract must own some toToken in order to proceed with the swap
     // We mint some for it by default for each tests
-    await toToken.mintSupplyFor(swapContract.address, new BN('1000'));
+    await mintInitialSupply(toToken, swapContract.address);
   });
 
   describe("provide()", () => {
@@ -34,19 +36,17 @@ contract("SwapContract", accounts => {
     });
 
     context("when the account has a sufficient balance of fromToken", () => {
-      const fromTokenBalance = new BN('1000');
+      beforeEach(async () => await mintInitialSupply(fromToken, account));
 
-      beforeEach(async () => await fromToken.mintSupplyFor(account, fromTokenBalance));
-
-      context("without approval", () => {
-        it("fails if the account hasn't approved allowance to the SwapContract first", async () => {
+      context("without giving approval to the contract", () => {
+        it("reverts the transaction if no approval at not", async () => {
           await expectRevert(
             swapContract.provide(depositValue, {from: account}),
             'ERC20: transfer amount exceeds allowance -- Reason given: ERC20: transfer amount exceeds allowance.'
           );
         });
 
-        it("fails if the account hasn't approved allowance for the total amount of fromToken to deposit", async () => {
+        it("reverts the transaction if not enough approval", async () => {
           const allowedValue = depositValue.subn(10);
           await fromToken.increaseAllowance(swapContract.address, allowedValue, {from: account});
 
@@ -58,29 +58,37 @@ contract("SwapContract", accounts => {
       });
 
       context("with approval", () => {
-        let swapContractFromTokenInitialBalance;
+        let initialContractBalance;
+        let initialAccountBalance;
 
         beforeEach(async () => {
           await fromToken.increaseAllowance(swapContract.address, depositValue, {from: account})
-          swapContractFromTokenInitialBalance = await fromToken.balanceOf(swapContract.address);
+          initialContractBalance = await fromToken.balanceOf(swapContract.address);
+          initialAccountBalance = await fromToken.balanceOf(account);
         });
 
         it("deposits the amount of fromToken to the contract after approving allowance", async () => {
           await swapContract.provide(depositValue, {from: account});
 
-          expect((await fromToken.balanceOf(account))).to.be.bignumber.equal((fromTokenBalance.sub(depositValue)));
-          expect((await fromToken.balanceOf(swapContract.address))).to.be.bignumber.equal((swapContractFromTokenInitialBalance.add(depositValue)));
+          expect(await fromToken.balanceOf(account)).to.be.bignumber.equal(initialAccountBalance.sub(depositValue));
+          expect(await fromToken.balanceOf(swapContract.address)).to.be.bignumber.equal(initialContractBalance.add(depositValue));
+        });
+
+        it("keeps the state contract up to date", async () => {
+          await swapContract.provide(depositValue, {from: account});
+
+          expect(await swapContract.balanceOfFromToken(account)).to.be.bignumber.equal(depositValue);
         });
 
         it("adds up multiple deposits", async () => {
           const [firstDeposit, secondDeposit] = [depositValue.divn(2), depositValue.divn(2)];
           await swapContract.provide(firstDeposit, {from: account});
           await swapContract.provide(secondDeposit, {from: account});
-          const remainingAccountBalance = fromTokenBalance.sub(firstDeposit.add(secondDeposit));
-          const contractBalance = firstDeposit.add(secondDeposit);
+          const expectedAccountBalance = initialAccountBalance.sub(firstDeposit.add(secondDeposit));
+          const expectedContractBalance = initialContractBalance.add(firstDeposit.add(secondDeposit));
 
-          expect((await fromToken.balanceOf(account))).to.be.bignumber.equal(remainingAccountBalance);
-          expect((await fromToken.balanceOf(swapContract.address))).to.be.bignumber.equal(contractBalance);
+          expect(await fromToken.balanceOf(account)).to.be.bignumber.equal(expectedAccountBalance);
+          expect(await fromToken.balanceOf(swapContract.address)).to.be.bignumber.equal(expectedContractBalance);
         });
       });
     });
